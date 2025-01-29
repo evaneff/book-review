78e4446b2d86d52ee85d2445e052714f2d585769
Eva Neff <evaneff@gmail.com>
2025-01-29 09:32:33 -0700

    finished star rating

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -24,11 +24,17 @@ class BookController extends Controller
             'popular_last_6months' => $books->popularLast6Months(),
             'highest_rated_last_month' => $books->highestRatedLastMonth(),
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
-            default => $books->latest()
+            default => $books->latest()->withAvgRating()->withReviewsCount()
         };
         //$books = $books->get();
         $cacheKey = 'books:' . $filter . ':' . $title;
-        $books = cache()->remember('cacheKey', 3600, fn() => $books->get());
+        $books =
+        //cache()->remember(
+        //  'cacheKey',
+        //    3600,
+        //    fn() =>
+            $books->get();
+        //    );
 
         return view('books.index', ['books' => $books]);
     }
@@ -52,17 +58,20 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Book $book)
+    public function show(int $id)
     {
+        $cacheKey = 'book:' . $id;
 
-        return view(
-            'books.show',
-            [
-                'book' => $book->load([
-                    'reviews' => fn ($query) => $query->latest()
-                ])
-            ]
+        $book = cache()->remember(
+            $cacheKey,
+            3600,
+            fn() =>
+            Book::with([
+                'reviews' => fn($query) => $query->latest()
+            ])->withAvgRating()->withReviewsCount()->findorFail($id)
         );
+
+        return view('books.show',['book' => $book]);
     }
 
     /**
